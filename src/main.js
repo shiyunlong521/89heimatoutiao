7f38e8a142af563c2bda01c781af9cc4263ded20
renhongda <renhongda101@163.com>
2019-12-21 18:43:07 +0800

    测试推送

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import ElementUI from 'element-ui' // 引入包
-import 'element-ui/lib/theme-chalk/index.css' // 引入elementui样式
-
-import './stylex.less'// 引入初始化样式
+import router from './router/index.js'
+import './styles/index.less'// 引入初始化样式
 Vue.config.productionTip = false
-Vue.use(ElementUI)// 全局注册
+
 new Vue({
   router,
   render: h => h(App)
---
+++ src/views/login/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="login">
+        <!-- 放置一个卡片组件 -->
+        <el-card class="login-card"></el-card>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+<style lang="less" scoped>
+// 写上scoped属性 那么只会对 当前的组件样式产生作用 原理 是 为当前的标签生成了 data-v-随机
+// 100vh相当于当前可视区域的100%高度
+.login {
+    background-image: url(../../img/login_bg.jpg);
+    background-size: cover;//自适应背景图片
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    .login-card {
+        width: 440px;
+        height: 350px;
+    }
+}
+</style>
